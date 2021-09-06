8fa86253a1d974d349c584256103fffcc686fe57
gscode <adm.guilhermedesousa@gmail.com>
2021-09-06 09:56:46 -0300

    19. 6.1. Testando um CRUD - exercicio

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,5 +1,11 @@
 it('successfully logs in', () => {
   cy.intercept('GET', '**/notes').as('getNotes')
-  cy.login()
+
+  cy.login(
+    Cypress.env('USER_EMAIL'),
+    Cypress.env('USER_PASSWORD'),
+    { cacheSession: false } // O uso do false é para executar o fluxo de login padrão (sem verificar a sessão)
+  )
+
   cy.wait('@getNotes')
 })
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,2 +1,3 @@
 import './commands'
 import 'cypress-mailosaur'
+import 'cypress-file-upload'
