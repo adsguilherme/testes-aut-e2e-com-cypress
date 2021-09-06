c6594c169114e1fabba1988a9cb0bd951de5413f
gscode <adm.guilhermedesousa@gmail.com>
2021-09-06 11:57:39 -0300

    20. 6.2. Testando um CRUD - exercício extra 1

--- cypress/integration/crud.spec.js
+++ cypress/integration/crud.spec.js
@@ -1,13 +1,21 @@
 it('CRUDs a note', () => {
   const faker = require('faker')
   const noteDescription = faker.lorem.words(4)
-
+  let attachFile = false 
+  // Foi utilizado let em vez de const, pois na linha 33 realizamos uma alteração de false para true
+  // false está indicando que não quero realizar o anexo
+  
   cy.intercept('GET', '**/notes').as('getNotes')
   cy.intercept('GET', '**/notes/**').as('getNote')
   cy.login()
 
   cy.visit('/notes/new')
   cy.get('#content').type(noteDescription)
+
+  if (attachFile) { // Se (if) o atachfile for 'true', encontra o elemento para realizar o upload.
+    cy.get('#file').attachFile('example.json') // Neste caso irá pegar o arquivo example.json, que está na pasta fixture.
+  }
+
   cy.contains('button', 'Create').click()
 
   cy.wait('@getNotes')
@@ -20,7 +28,14 @@ it('CRUDs a note', () => {
 
   cy.get('#content')
     .clear()
-    .type(updatedNoteDescription)
+    .type(updatedNoteDescription) // após o update será anexo o arquivo
+
+  attachFile = true
+
+  if (attachFile) {
+    cy.get('#file').attachFile('example.json')
+  }
+
   cy.contains('button', 'Save').click()
   cy.wait('@getNotes')
 
